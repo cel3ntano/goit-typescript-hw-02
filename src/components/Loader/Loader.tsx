c9e6098625225abba732145d9e26b8087e0314c0
cel3ntano <adriano2024celentano@gmail.com>
2024-08-27 21:28:03 +0200

    Loader typification

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,7 +1,11 @@
 import { ThreeDots } from "react-loader-spinner";
 import css from "./Loader.module.css";
 
-export default function Loader({ isLoading }) {
+interface LoaderProps {
+  isLoading: boolean;
+}
+
+export default function Loader({ isLoading }: LoaderProps): JSX.Element {
   return (
     <ThreeDots
       visible={isLoading}
